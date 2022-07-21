192d7910b39625e0272860f21cd1bf583d69e51c
moocss <moocss@163.com>
2022-07-21 17:49:44 +0800

    add: v1.4.0

--- config/config.go
+++ config/config.go
@@ -1,156 +1,266 @@
 package config
 
 import (
-	"context"
 	"errors"
-	"reflect"
-	"sync"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
-	// init encoding
-	_ "github.com/apus-run/gaia/encoding/json"
-	_ "github.com/apus-run/gaia/encoding/proto"
-	_ "github.com/apus-run/gaia/encoding/xml"
-	_ "github.com/apus-run/gaia/encoding/yaml"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+
+	"github.com/apus-run/gaia/config/file"
 	"github.com/apus-run/gaia/log"
 )
 
 var (
-	// ErrNotFound is key not found.
-	ErrNotFound = errors.New("key not found")
+	defaultFile = "sea"
+	files       map[string]*config
 
 	_ Config = (*config)(nil)
 )
 
-// Observer is config observer.
-type Observer func(string, Value)
-
 // Config is a config interface.
 type Config interface {
 	Load() error
-	Scan(v interface{}) error
-	Value(key string) Value
-	Watch(key string, o Observer) error
-	Close() error
+	Watch()
 }
+
+// ConfigOption is config option
+type ConfigOption func(*config)
 
 type config struct {
-	opts      options
-	reader    Reader
-	cached    sync.Map
-	observers sync.Map
-	watchers  []Watcher
+	sources []*file.File
+	viper   *viper.Viper
+
+	log *log.Helper
 }
 
-// New new a config with options.
-func New(opts ...Option) Config {
-	o := options{
-		decoder:  defaultDecoder,
-		resolver: defaultResolver,
+// WithSource with config source.
+func WithSource(s ...*file.File) ConfigOption {
+	return func(o *config) {
+		o.sources = s
 	}
-	for _, opt := range opts {
-		opt(&o)
+}
+
+// WithLogger with server logger.
+func WithLogger(logger log.Logger) ConfigOption {
+	return func(s *config) {
+		s.log = log.NewHelper(logger)
 	}
+}
+
+// defaultOptions .
+func defaultOptions() *config {
 	return &config{
-		opts:   o,
-		reader: newReader(o),
+		log: log.NewHelper(log.GetLogger()),
 	}
 }
 
-func (c *config) watch(w Watcher) {
-	for {
-		kvs, err := w.Next()
-		if errors.Is(err, context.Canceled) {
-			log.Infof("watcher's ctx cancel : %v", err)
-			return
-		}
+// New a config with options.
+func New(opts ...ConfigOption) Config {
+	c := defaultOptions()
+	for _, o := range opts {
+		o(c)
+	}
+
+	return c
+}
+
+func (c *config) Load() error {
+	for _, src := range c.sources {
+		fs, err := src.Load()
 		if err != nil {
-			time.Sleep(time.Second)
-			log.Errorf("failed to watch next config: %v", err)
-			continue
+			return err
 		}
-		if err := c.reader.Merge(kvs...); err != nil {
-			log.Errorf("failed to merge next config: %v", err)
-			continue
-		}
-		if err := c.reader.Resolve(); err != nil {
-			log.Errorf("failed to resolve next config: %v", err)
-			continue
-		}
-		c.cached.Range(func(key, value interface{}) bool {
-			k := key.(string)
-			v := value.(Value)
-			if n, ok := c.reader.Value(k); ok && reflect.TypeOf(n.Load()) == reflect.TypeOf(v.Load()) && !reflect.DeepEqual(n.Load(), v.Load()) {
-				v.Store(n.Load())
-				if o, ok := c.observers.Load(k); ok {
-					o.(Observer)(k, v)
-				}
+		files = make(map[string]*config, len(fs))
+		for _, f := range fs {
+			v, err := c.load(f)
+			if err != nil {
+				return err
 			}
-			return true
+			name := strings.TrimSuffix(path.Base(f), filepath.Ext(f))
+			// c.log.Infof("文件名: %s", name)
+			files[name] = &config{
+				viper: v,
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *config) load(fileName string) (*viper.Viper, error) {
+	c.log.Infof("配置文件: %s", fileName)
+	suffix := filepath.Ext(fileName)
+	fileType := strings.TrimPrefix(suffix, ".")
+
+	v := viper.New()
+
+	v.SetConfigFile(fileName)
+	v.SetConfigType(fileType)
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			c.log.Warnf("Using config file: %s [%s]\n", viper.ConfigFileUsed(), err)
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	// 读取匹配的环境变量
+	v.AutomaticEnv()
+
+	return v, nil
+}
+
+func (c *config) Watch() {
+	for _, v := range files {
+		v.viper.WatchConfig()
+		v.viper.OnConfigChange(func(e fsnotify.Event) {
+			fmt.Printf("Config file changed: %s \n", e.Name)
 		})
 	}
 }
 
-func (c *config) Load() error {
-	for _, src := range c.opts.sources {
-		kvs, err := src.Load()
+// GetFloat64 获取浮点数配置
+func GetFloat64(key string) float64 { return File(defaultFile).GetFloat64(key) }
+func (c *config) GetFloat64(key string) float64 {
+	return c.viper.GetFloat64(key)
+}
+
+// Get 获取字符串配置
+func Get(key string) string { return File(defaultFile).Get(key) }
+func (c *config) Get(key string) string {
+	return c.viper.GetString(key)
+}
+
+// GetStrings 获取字符串列表
+func GetStrings(key string) (s []string) { return File(defaultFile).GetStrings(key) }
+func (c *config) GetStrings(key string) (s []string) {
+	value := Get(key)
+	if value == "" {
+		return
+	}
+
+	for _, v := range strings.Split(value, ",") {
+		s = append(s, v)
+	}
+	return
+}
+
+// GetInt32s 获取数字列表
+// 1,2,3 => []int32{1,2,3}
+func GetInt32s(key string) (s []int32, err error) { return File(defaultFile).GetInt32s(key) }
+func (c *config) GetInt32s(key string) (s []int32, err error) {
+	s64, err := GetInt64s(key)
+	for _, v := range s64 {
+		s = append(s, int32(v))
+	}
+	return
+}
+
+// GetInt64s 获取数字列表
+func GetInt64s(key string) (s []int64, err error) { return File(defaultFile).GetInt64s(key) }
+func (c *config) GetInt64s(key string) (s []int64, err error) {
+	value := Get(key)
+	if value == "" {
+		return
+	}
+
+	var i int64
+	for _, v := range strings.Split(value, ",") {
+		i, err = strconv.ParseInt(v, 10, 64)
 		if err != nil {
-			return err
+			return
 		}
-		for _, v := range kvs {
-			log.Debugf("config loaded: %s format: %s", v.Key, v.Format)
-		}
-		if err = c.reader.Merge(kvs...); err != nil {
-			log.Errorf("failed to merge config source: %v", err)
-			return err
-		}
-		w, err := src.Watch()
-		if err != nil {
-			log.Errorf("failed to watch config source: %v", err)
-			return err
-		}
-		c.watchers = append(c.watchers, w)
-		go c.watch(w)
+		s = append(s, i)
 	}
-	if err := c.reader.Resolve(); err != nil {
-		log.Errorf("failed to resolve config source: %v", err)
-		return err
-	}
-	return nil
+	return
 }
 
-func (c *config) Value(key string) Value {
-	if v, ok := c.cached.Load(key); ok {
-		return v.(Value)
-	}
-	if v, ok := c.reader.Value(key); ok {
-		c.cached.Store(key, v)
-		return v
-	}
-	return &errValue{err: ErrNotFound}
+// GetInt 获取整数配置
+func GetInt(key string) int { return File(defaultFile).GetInt(key) }
+func (c *config) GetInt(key string) int {
+	return c.viper.GetInt(key)
 }
 
-func (c *config) Scan(v interface{}) error {
-	data, err := c.reader.Source()
-	if err != nil {
-		return err
-	}
-	return unmarshalJSON(data, v)
+// GetInt32 获取 int32 配置
+func GetInt32(key string) int32 { return File(defaultFile).GetInt32(key) }
+func (c *config) GetInt32(key string) int32 {
+	return c.viper.GetInt32(key)
 }
 
-func (c *config) Watch(key string, o Observer) error {
-	if v := c.Value(key); v.Load() == nil {
-		return ErrNotFound
-	}
-	c.observers.Store(key, o)
-	return nil
+// GetInt64 获取 int64 配置
+func GetInt64(key string) int64 { return File(defaultFile).GetInt64(key) }
+func (c *config) GetInt64(key string) int64 {
+	return c.viper.GetInt64(key)
 }
 
-func (c *config) Close() error {
-	for _, w := range c.watchers {
-		if err := w.Stop(); err != nil {
-			return err
-		}
+// GetDuration 获取时间配置
+func GetDuration(key string) time.Duration { return File(defaultFile).GetDuration(key) }
+func (c *config) GetDuration(key string) time.Duration {
+	return c.viper.GetDuration(key)
+}
+
+// GetTime 查询时间配置
+// 默认时间格式为 "2006-01-02 15:04:05"，conf.GetTime("FOO_BEGIN")
+// 如果需要指定时间格式，则可以多传一个参数，conf.GetString("FOO_BEGIN", "2006")
+//
+// 配置不存在或时间格式错误返回**空时间对象**
+// 使用本地时区
+func GetTime(key string, args ...string) time.Time { return File(defaultFile).GetTime(key, args...) }
+func (c *config) GetTime(key string, args ...string) time.Time {
+	d := "2006-01-02 15:04:05"
+	if len(args) == 1 {
+		d = args[0]
 	}
-	return nil
+
+	t, _ := time.ParseInLocation(d, c.viper.GetString(key), time.Local)
+	return t
+}
+
+// GetBool 获取配置布尔配置
+func GetBool(key string) bool { return File(defaultFile).GetBool(key) }
+func (c *config) GetBool(key string) bool {
+	return c.viper.GetBool(key)
+}
+
+// Sub 返回新的Viper实例，代表该实例的子节点。
+func Sub(key string) (*viper.Viper, error) { return File(defaultFile).Sub(key) }
+func (c *config) Sub(key string) (*viper.Viper, error) {
+	if app := c.viper.Sub(key); app != nil {
+		return app, nil
+	}
+	return nil, fmt.Errorf("No found `%s` in the configuration", key)
+}
+
+// Set 设置配置，仅用于测试
+func Set(key string, value interface{}) { File(defaultFile).Set(key, value) }
+func (c *config) Set(key string, value interface{}) {
+	c.viper.Set(key, value)
+}
+
+// File 根据文件名获取对应配置对象
+// 如果要读取 foo.yaml 配置，可以 File("foo").Get("bar")
+func File(name string) *config {
+	res, _ := files[name]
+	if res == nil {
+		res = &config{viper: &viper.Viper{}}
+	}
+	return res
+}
+
+// GetEnvString get value from env.
+// application parameters take precedence over environment variables
+// env := GetEnvString("APP_ENV", "")
+func GetEnvString(key string, defaultValue string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return val
 }
--- config/file/file.go
+++ config/file/file.go
@@ -1,52 +1,42 @@
 package file
 
 import (
-	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/apus-run/gaia/config"
 )
 
-var _ config.Source = (*file)(nil)
-
-type file struct {
+type File struct {
 	path string
 }
 
-// NewSource new a file source.
-func NewSource(path string) config.Source {
-	return &file{path: path}
+// NewFile new a file source.
+func NewFile(path string) *File {
+	return &File{path: path}
 }
 
-func (f *file) loadFile(path string) (*config.KeyValue, error) {
+func (f *File) loadFile(path string) (string, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
+
 	defer func() {
 		if err := file.Close(); err != nil {
 			panic(err)
 		}
 	}()
-	data, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
+
 	info, err := file.Stat()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return &config.KeyValue{
-		Key:    info.Name(),
-		Format: format(info.Name()),
-		Value:  data,
-	}, nil
+	return info.Name(), nil
 }
 
-func (f *file) loadDir(path string) (kvs []*config.KeyValue, err error) {
-	files, err := os.ReadDir(path)
+func (f *File) loadDir(path string) (sources []string, err error) {
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		return nil, err
 	}
@@ -55,16 +45,18 @@ func (f *file) loadDir(path string) (kvs []*config.KeyValue, err error) {
 		if file.IsDir() || strings.HasPrefix(file.Name(), ".") {
 			continue
 		}
-		kv, err := f.loadFile(filepath.Join(path, file.Name()))
+
+		p := filepath.Join(path, file.Name())
+		_, err = f.loadFile(p)
 		if err != nil {
 			return nil, err
 		}
-		kvs = append(kvs, kv)
+		sources = append(sources, p)
 	}
 	return
 }
 
-func (f *file) Load() (kvs []*config.KeyValue, err error) {
+func (f *File) Load() (sources []string, err error) {
 	fi, err := os.Stat(f.path)
 	if err != nil {
 		return nil, err
@@ -72,13 +64,11 @@ func (f *file) Load() (kvs []*config.KeyValue, err error) {
 	if fi.IsDir() {
 		return f.loadDir(f.path)
 	}
-	kv, err := f.loadFile(f.path)
+
+	_, err = f.loadFile(f.path)
 	if err != nil {
 		return nil, err
 	}
-	return []*config.KeyValue{kv}, nil
-}
-
-func (f *file) Watch() (config.Watcher, error) {
-	return newWatcher(f)
+	// fmt.Printf("根路径: %s\n", f.path)
+	return []string{f.path}, nil
 }
